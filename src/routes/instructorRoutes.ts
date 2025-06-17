ed685d45132acdc9881c56b3201ba998d3c50e78
samuel-prata <samuel.silverio001@hotmail.com>
2025-06-17 19:27:48 -0300

    test: testing github actions

--- src/routes/instructorRoutes.ts
+++ src/routes/instructorRoutes.ts
@@ -2,8 +2,9 @@ import { Router } from "express";
 import { instructorController } from "../controllers/instructorController";
 
 const instructorRouter = Router();
-instructorRouter.get("/", instructorController.getAllInstructors);
 
+instructorRouter.get("/", instructorController.getAllInstructors);
+// Testando Ci
 instructorRouter.post("/", instructorController.create);
 
 export default instructorRouter;
