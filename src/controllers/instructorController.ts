a3be4f3b4f80ecb9141c642dc2c968fe2355f813
samuel-prata <samuel.silverio001@hotmail.com>
2025-06-20 19:11:53 -0300

    fix (controller): Fixing controller instructors

--- src/controllers/instructorController.ts
+++ src/controllers/instructorController.ts
@@ -2,29 +2,33 @@ import { Request, Response } from "express";
 import { InstructorFactory } from "../factories/instructorFactory";
 import { Instructor } from "../models/instructorModel";
 import { instructorValidation } from "../middleware/instructorMiddleware";
+
 const instructors: Instructor[] = [];
 
 export const instructorController = {
-
-    getAllInstructors: (req: Request, res: Response) => {
+    getAllInstructors: (req: Request, res: Response): void => {
         res.status(200).json(instructors);
     },
 
-    create: (req: Request, res: Response) => {
-        const instructor = instructorValidation.parse(req.body);
-        if (!instructor) {
-            return res.status(400).json({ message: "Invalid instructor data" });
-        }
+    create: (req: Request, res: Response): void => {
         try {
-            instructors.push(InstructorFactory.createInstructor(instructor.name, instructor.email, instructor.salary, instructor.expertise));
+            const instructor = instructorValidation.parse(req.body);
+            instructors.push(
+                InstructorFactory.createInstructor(
+                    instructor.name,
+                    instructor.email,
+                    instructor.salary,
+                    instructor.expertise
+                )
+            );
+            res.status(201).json({
+                message: "Instructor created successfully",
+                instructors: instructors,
+            });
         } catch (err) {
-            return res.status(500).json({ message: "Error creating instructor", error: err});
+            res
+                .status(500)
+                .json({ message: "Error creating instructor", error: err });
         }
-        
-        res.status(201).json({
-            message: "Instructor created successfully",
-            instructors: instructors
-        })
-
-    }
-}
+    },
+};
